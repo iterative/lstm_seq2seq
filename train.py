ff99cdb9f256c26bddf241f71a379e9ca6a6c211
dberenbaum <dave@iterative.ai>
2023-07-03 14:38:03 -0400

    dvc: commit experiment 81cf6f21dba37bd46063ac10b6cf3f9db32bd6e6eaa464fd64c91b4d741fde24

--- train.py
+++ train.py
@@ -161,15 +161,9 @@ val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size)
 
 exp = Live("results", save_dvc_exp=True)
 live = DVCLiveLogger(report=None, experiment=exp)
-checkpoint = pl.callbacks.ModelCheckpoint(
-        dirpath="model",
-        filename="model",
-        monitor="val_acc",
-        mode="max",
-        save_weights_only=True, every_n_epochs=1)
 timer = pl.callbacks.Timer(duration=params["model"]["duration"])
 
 trainer = pl.Trainer(max_epochs=params["model"]["max_epochs"], logger=[live],
-                     callbacks=[timer, checkpoint])
+                     callbacks=[timer])
 trainer.fit(model=arch, train_dataloaders=train_loader,
         val_dataloaders=val_loader)
